89a6da9dc0dbccadc9a74b772827cb94fa2b3c83
suraj-repositories <suraj2002mail@gmail.com>
2025-11-24 08:01:12 +0530

    updated get funciton

--- api/device/get.php
+++ api/device/get.php
@@ -13,7 +13,13 @@ if ($_SERVER['REQUEST_METHOD'] !== 'GET') {
     exit;
 }
 
-$stmt = mysqli_prepare($conn, "SELECT * FROM devices WHERE user_id = ? ORDER BY name ASC LIMIT 200");
+$getQuery = "
+SELECT devices.*,device_types.name as device_type_name, device_types.type_key as device_type FROM devices
+INNER JOIN device_types ON devices.device_type_id = device_types.id
+ WHERE user_id = ? 
+ ORDER BY name ASC LIMIT 200
+";
+$stmt = mysqli_prepare($conn, $getQuery);
 mysqli_stmt_bind_param($stmt, 'i', $user_id);
 mysqli_stmt_execute($stmt);
 $result = mysqli_stmt_get_result($stmt);
